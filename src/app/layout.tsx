7b62b73b010026fd134ec0157abc96f1e0695a11
Wallace Duarte <wd.wallaceduarte@gmail.com>
2026-03-06 00:27:16 +0000

    feat: implementar estutura basica raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from 'next'
+import { inter } from './ui/fonts'
+import './globals.css'
+
+export const metadata: Metadata = {
+    title: 'ACME'
+}
+
+export default function RootLayout(
+    { children }: { children: React.ReactNode }
+) {
+    return (
+        <html>
+            <body className={ inter.className }>
+                { children }
+            </body>
+        </html>
+    )
+}
