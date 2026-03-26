d8996d00bb043927d06ba575d1ed6800006ae3f9
Wallace Duarte <wd.wallaceduarte@gmail.com>
2026-03-26 22:44:35 +0000

    feat: definir contrato de tipos para usuarios, clientes, faturas, receita, responsta da api e respostar de retorino e erro da api

---
+++ src/types/index.ts
@@ -0,0 +1,46 @@
+export interface User {
+    id: string;
+    nome: string;
+    email: string;
+    password: string;
+}
+
+export type CreatUserData = Omit<User, 'id'>
+export type UpdateUserData = Partial<CreatUserData>
+
+export interface Customer {
+    id: string;
+    name: string:
+    email: string;
+    imageUrl: string;
+}
+
+export type CreatCustomerData = Omit<Customer, 'id'>
+export type UpdateCustomerData = Partial<CreatCustomerData>
+
+export type InvoiceStatus = 'PAGO' | 'PENDENTE'
+export interface Invoice {
+    id: string;
+    amount: number;
+    date: Date;
+    status: InvoiceStatus;
+    customer_id: string;
+}
+
+export type CreatInvoiceData = Omit<Invoice, 'id'>
+export type UpdateInvoiceData = Partial<CreatInvoiceData>
+
+export interface Revenue {
+    month: string;
+    revenue: number;
+}
+
+export interface ApiResponse<GENERIC> {
+    data: GENERIC;
+    massage?: string;
+}
+
+export interface ApiError {
+    error: string;
+    details: unknown;
+}
